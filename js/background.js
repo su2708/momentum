2d4a7a8184638ecf5d117caf6fe8cb59ddb71688
su2708 <46125751+su2708@users.noreply.github.com>
2021-09-22 09:53:35 +0000

    Create bg.js and quote.js

---
+++ js/background.js
@@ -0,0 +1,7 @@
+const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+const bgImage = document.createElement("img");
+
+bgImage.src = `images/${chosenImage}`;
+
+document.body.appendChild(bgImage);
