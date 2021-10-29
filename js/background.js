426c28271eca064975c27e57f65826f038f35149
su2708 <46125751+su2708@users.noreply.github.com>
2021-10-29 14:29:55 +0000

    Initial design

--- js/background.js
+++ js/background.js
@@ -1,7 +1,15 @@
-const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg", "16.jpg", "17.jpg", "18.jpg", "19.jpg", "20.jpg"];
+const images = [
+  "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", 
+  "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", 
+  "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg", 
+  "16.jpg", "17.jpg", "18.jpg", "19.jpg", "20.jpg"
+  ];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 
 bgImage.src = `images/${chosenImage}`;
 
-document.body.style = `background-image: url(images/${chosenImage}); background-size: 100% 100%;`;
+document.body.style = `
+background-image: url(images/${chosenImage}); 
+background-size: 100% 100%;
+`;
--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,6 @@
 const API_KEY = "65f7af0c069d9b5c9486960b40e776a2";
 
-function onGeoOk(position) {
+function currentGeo(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
@@ -14,4 +14,4 @@ function onGeoOk(position) {
     });
 }
 
-navigator.geolocation.getCurrentPosition(onGeoOk);
+navigator.geolocation.getCurrentPosition(currentGeo);
